8675db386750f4793a319f3775afaf5c0ca84476
Shaun W Colley <swc7838@gmail.com>
2019-04-18 15:12:47 -0500

    portfolio close to done

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const mustacheExpress = require('mustache-express')
+const PORT = process.env.PORT || 8080
+
+
+app.engine('mustache',mustacheExpress())
+app.set('views','./views')
+app.set('view engine','mustache')
+app.use(express.static('css'))
+app.use(express.static('images'))
+
+app.get('/', (req,res) => {
+  res.render('home')
+})
+
+app.listen(PORT,function(){
+  console.log("Prepare for the volley... Served.")
+})
